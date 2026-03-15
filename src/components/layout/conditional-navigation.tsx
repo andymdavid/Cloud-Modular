724568522a7fc17b29c759911ba5103707ffe84c
andymdavid <andy@otherstuff.studio>
2026-03-15 14:51:16 +0800

    Use navigation-v2 as site header

--- src/components/layout/conditional-navigation.tsx
+++ src/components/layout/conditional-navigation.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import { Navigation } from "./navigation";
+import { NavigationV2 } from "./navigation-v2";
 
 export function ConditionalNavigation() {
-  const pathname = usePathname();
-
-  // Don't render global navigation on the v2 homepage or its working route.
-  if (pathname === "/" || pathname?.startsWith("/home-v2")) {
-    return null;
-  }
-
-  return <Navigation />;
+  return <NavigationV2 />;
 }
