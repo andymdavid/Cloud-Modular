c678e55d504236312feb169c568ff4008c5eac3d
andymdavid <andy@otherstuff.studio>
2026-04-06 18:25:06 +0800

    Restore modular section divider

--- src/app/home-v2/page.tsx
+++ src/app/home-v2/page.tsx
@@ -611,6 +611,7 @@ export default function HomeV2() {
             </div>
           </div>
         </div>
+        <div className={cn("relative left-1/2 w-screen -translate-x-1/2 border-b", borderDivider)}></div>
       </section>
 
       <section className={cn("min-h-[118svh] flex flex-col overflow-hidden sm:h-screen sm:min-h-0", bg, text)}>
